ce95c48d8260855ebb5efe52f712a6ac931f81a0
seveibar <seveibar@gmail.com>
2025-06-23 12:02:47 -0700

    add basic test

---
+++ tests/test01.test.ts
@@ -0,0 +1,30 @@
+import { test, expect } from "bun:test";
+import { calculateCellBoundaries } from "../lib";
+
+const scene = {
+	cellContents: [
+		{
+			minX: 0,
+			minY: 0,
+			maxX: 100,
+			maxY: 100,
+		},
+		{
+			minX: 200,
+			minY: 0,
+			maxX: 300,
+			maxY: 100,
+		},
+	],
+};
+
+test("test01", () => {
+	const boundaries = calculateCellBoundaries(scene.cellContents);
+
+	expect(boundaries).toEqual([
+		{
+			start: { x: 150, y: 0 },
+			end: { x: 150, y: 100 },
+		},
+	]);
+});
