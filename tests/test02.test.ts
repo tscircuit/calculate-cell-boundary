ee0e72357e00ee720900ebfb3d2c0be6a4ef0eb9
seveibar <seveibar@gmail.com>
2025-06-23 12:04:08 -0700

    add second test with vertical block

---
+++ tests/test02.test.ts
@@ -0,0 +1,30 @@
+import { test, expect } from "bun:test";
+import { calculateCellBoundaries } from "../lib";
+
+const scene = {
+	cellContents: [
+		{
+			minX: 0,
+			minY: 0,
+			maxX: 100,
+			maxY: 100,
+		},
+		{
+			minX: 0,
+			minY: 200,
+			maxX: 100,
+			maxY: 300,
+		},
+	],
+};
+
+test("test02", () => {
+	const boundaries = calculateCellBoundaries(scene.cellContents);
+
+	expect(boundaries).toEqual([
+		{
+			start: { x: 0, y: 150 },
+			end: { x: 100, y: 150 },
+		},
+	]);
+});
