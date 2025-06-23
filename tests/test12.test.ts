db3717159c4e3567c4e6f7b76b412d24f38b626f
seveibar <seveibar@gmail.com>
2025-06-23 16:36:45 -0700

    add test12

---
+++ tests/test12.test.ts
@@ -0,0 +1,70 @@
+import { test, expect } from "bun:test"
+import { calculateCellBoundaries } from "../lib"
+
+const scene = {
+  cellContents: [
+    {
+      minX: 175,
+      minY: 50,
+      maxX: 275,
+      maxY: 150,
+    },
+    {
+      minX: 25,
+      minY: 225,
+      maxX: 125,
+      maxY: 325,
+    },
+    {
+      minX: 250,
+      minY: 300,
+      maxX: 350,
+      maxY: 400,
+    },
+    {
+      minX: 375,
+      minY: 75,
+      maxX: 475,
+      maxY: 175,
+    },
+  ],
+}
+
+test("test12", () => {
+  const boundaries = calculateCellBoundaries(scene.cellContents)
+
+  expect(boundaries).toEqual(
+    [
+      {
+        start: {
+          x: 187.5,
+          y: 225,
+        },
+        end: {
+          x: 187.5,
+          y: 400,
+        },
+      },
+      {
+        start: {
+          x: 187.5,
+          y: 225,
+        },
+        end: {
+          x: 475,
+          y: 225,
+        },
+      },
+      {
+        start: {
+          x: 325,
+          y: 50,
+        },
+        end: {
+          x: 325,
+          y: 200,
+        },
+      },
+    ].sort((a, b) => JSON.stringify(a).localeCompare(JSON.stringify(b))),
+  )
+})
